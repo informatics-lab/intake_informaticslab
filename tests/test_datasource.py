ab5e2f5ef46ec59598874ef6f55937da9e898f11
Aaron Hopkinson <arh89@users.noreply.github.com>
2020-11-12 15:24:35 +0000

    Fix broken tests for MetOfficeDataSource (#7)

--- tests/test_datasource.py
+++ tests/test_datasource.py
@@ -11,11 +11,23 @@ def test_init():
         cycle_frequency="1H",
         forecast_extent="1H",
         model="mo-atmospheric-mogreps-uk",
+        dimensions=[
+            "forecast_reference_time",
+            "forecast_period",
+            "realization",
+            "projection_y_coordinate",
+            "projection_x_coordinate",
+        ],
         diagnostics=["temperature_at_screen_level"],
+        static_coords={
+            "realization": {"data": [0, 1, 2]},
+            "projection_y_coordinate": {"data": {"start": 100, "stop": 200, "num": 10}},
+            "projection_x_coordinate": {"data": {"start": 100, "stop": 200, "num": 10}},
+        },
         storage_options={
-            'data_protocol': 'file',
-            'url_prefix': '/tmp/',
-        }
+            "data_protocol": "file",
+            "url_prefix": "/tmp/",
+        },
     )
 
 
@@ -29,11 +41,23 @@ def test_get_some_data():
         cycle_frequency="1H",
         forecast_extent="1H",
         model="mo-atmospheric-mogreps-uk",
+        dimensions=[
+            "forecast_reference_time",
+            "forecast_period",
+            "realization",
+            "projection_y_coordinate",
+            "projection_x_coordinate",
+        ],
         diagnostics=["temperature_at_screen_level"],
+        static_coords={
+            "realization": {"data": [0, 1, 2]},
+            "projection_y_coordinate": {"data": {"start": 100, "stop": 200, "num": 10}},
+            "projection_x_coordinate": {"data": {"start": 100, "stop": 200, "num": 10}},
+        },
         storage_options={
-            'data_protocol': 'file',
-            'url_prefix': '/tmp/',
-        }
+            "data_protocol": "file",
+            "url_prefix": "/tmp/",
+        },
     )
     data = ds.read_chunked()
     assert isinstance(data, xr.Dataset) == True
