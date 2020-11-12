dcbfd93987ac6f288b20ab659c4e0d7a526aaac9
Theo McCaie <theo.mccaie@informaticslab.co.uk>
2020-11-12 10:42:39 +0000

    Update met_office_datasets/__init__.py
    Co-authored-by: Aaron Hopkinson <arh89@users.noreply.github.com>

--- met_office_datasets/__init__.py
+++ met_office_datasets/__init__.py
@@ -14,6 +14,6 @@ import intake
 from .datasources import MetOfficeDataSource
 
 mogreps_cat = intake.open_catalog(os.path.join(os.path.dirname(__file__), 'mogreps_uk_cat.yaml'))
-aq_cat = intake.open_catalog(os.path.join(os.path.dirname(__file__), 'mogreps_uk_cat.yaml'))
+aq_cat = intake.open_catalog(os.path.join(os.path.dirname(__file__), 'air_quality_cat.yaml'))
 # fmt: on
 # autopep8: on
