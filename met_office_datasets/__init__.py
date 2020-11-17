f7405791612ed36ae1dfddea5c717da89db92cd9
Aaron Hopkinson <aaron.hopkinson@informaticslab.co.uk>
2020-11-17 13:08:01 +0000

    Moved catalogs to separate directory

--- met_office_datasets/__init__.py
+++ met_office_datasets/__init__.py
@@ -13,12 +13,14 @@ import intake
 
 from .datasources import MetOfficeDataSource, MergedMetOfficeDataSource
 
-mogreps_cat = intake.open_catalog(os.path.join(os.path.dirname(__file__), 'mogreps_uk_cat.yaml'))
-aq_cat = intake.open_catalog(os.path.join(os.path.dirname(__file__), 'air_quality_cat.yaml'))
+CATALOG_DIR = os.path.join(os.path.dirname(__file__), 'cats')
+
+mogreps_cat = intake.open_catalog(os.path.join(CATALOG_DIR, 'mogreps_uk_cat.yaml'))
+aq_cat = intake.open_catalog(os.path.join(CATALOG_DIR, 'air_quality_cat.yaml'))
 
 cat = intake.catalog.Catalog.from_dict(
     {
-        'air_quality':aq_cat, 
+        'air_quality':aq_cat,
         'weather_forecasts':mogreps_cat
     }, name="Met Office Datasets")
 
