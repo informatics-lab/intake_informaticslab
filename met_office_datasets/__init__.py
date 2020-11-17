4711fe07e28af337cb60ae535c1378da7224355e
Aaron Hopkinson <aaron.hopkinson@informaticslab.co.uk>
2020-11-17 12:42:55 +0000

    Add support for merging MetOfficeDataSources (using xarray.Dataset.merge)

--- met_office_datasets/__init__.py
+++ met_office_datasets/__init__.py
@@ -11,7 +11,7 @@ import os
 
 import intake
 
-from .datasources import MetOfficeDataSource
+from .datasources import MetOfficeDataSource, MergedMetOfficeDataSource
 
 mogreps_cat = intake.open_catalog(os.path.join(os.path.dirname(__file__), 'mogreps_uk_cat.yaml'))
 aq_cat = intake.open_catalog(os.path.join(os.path.dirname(__file__), 'air_quality_cat.yaml'))
